2764c27ccca21e547faa1b9fd9df58a2326e2058
AshimChoudhary <panghalashim7832@gmail.com>
2025-07-23 11:10:44 +0530

    Navbar Progress

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <Link to="/" className="max-md:flex-1">
           <img src={assets.logo} className="w-36 h-auto" alt="" />
         </Link>
-
+        a
         <div
           className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${
             isOpen ? 'max-md:w-full' : 'max-md:w-0'
@@ -62,7 +62,6 @@ const Navbar = () => {
             Favourites
           </Link>
         </div>
-
         <div className="flex items-center gap-8">
           <SearchIcon className="max-md:hidden size-6 cursor-pointer" />
           <button className="px-4 py-1 rounded-full font-medium cursor-pointer sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition">
