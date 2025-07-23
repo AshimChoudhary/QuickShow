e59e056006c6feae34e22e191e75c4b752c9257e
AshimChoudhary <panghalashim7832@gmail.com>
2025-07-23 22:59:07 +0530

    Trailer Section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FeaturedSection from '../components/FeaturedSection';
 import HeroSection from '../components/HeroSection';
+import TrailorSection from '../components/TrailorSection';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <FeaturedSection />
+      <TrailorSection />
     </>
   );
 };
