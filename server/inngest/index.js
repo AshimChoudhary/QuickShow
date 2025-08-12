b759999b20e672f58fcf33354cc9452e93bfa88c
AshimChoudhary <panghalashim7832@gmail.com>
2025-08-12 20:17:18 +0530

    Changes

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,59 +3,43 @@ import User from '../models/User.js';
 
 export const inngest = new Inngest({ id: 'movie-ticket-booking' });
 
-const syncUserCreation = inngest.createFunction(
-  { id: 'sync-user-from-clerk' },
+const userCreated = inngest.createFunction(
+  { id: 'create-user' },
   { event: 'clerk/user.created' },
   async ({ event }) => {
-    try {
-      const { id, first_name, last_name, email_addresses, image_url } =
-        event.data;
-      const userData = {
-        clerkId: id,
-        email: email_addresses[0]?.email_address,
-        name: [first_name, last_name].filter(Boolean).join(' '),
-        image: image_url,
-      };
-      await User.create(userData);
-    } catch (err) {
-      console.error('User creation failed:', err);
-      throw err;
-    }
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userdata = {
+      _id: id,
+      name: first_name + ' ' + last_name,
+      email: email_addresses[0].email_address,
+      image: image_url,
+    };
+    await User.create(userdata);
   }
 );
-
-const syncUserDeletion = inngest.createFunction(
-  { id: 'delete-user-with-clerk' },
-  { event: 'clerk/user.deleted' },
-  async ({ event }) => {
-    try {
-      const { id } = event.data;
-      await User.findOneAndDelete({ clerkId: id });
-    } catch (err) {
-      console.error('User deletion failed:', err);
-      throw err;
-    }
-  }
-);
-
-const syncUserUpdate = inngest.createFunction(
-  { id: 'update-user-from-clerk' },
+const userUpdated = inngest.createFunction(
+  { id: 'update-user' },
   { event: 'clerk/user.updated' },
   async ({ event }) => {
-    try {
-      const { id, first_name, last_name, email_addresses, image_url } =
-        event.data;
-      const userData = {
-        email: email_addresses[0]?.email_address,
-        name: [first_name, last_name].filter(Boolean).join(' '),
-        image: image_url,
-      };
-      await User.findOneAndUpdate({ clerkId: id }, userData);
-    } catch (err) {
-      console.error('User update failed:', err);
-      throw err;
-    }
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userdata = {
+      _id: id,
+      name: first_name + ' ' + last_name,
+      email: email_addresses[0].email_address,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userdata);
+  }
+);
+const userDeleted = inngest.createFunction(
+  { id: 'delete-user' },
+  { event: 'clerk/user.deleted' },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
   }
 );
 
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate];
+export const functions = [userCreated, userUpdated, userDeleted];
