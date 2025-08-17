b4a535a8c5905050bb15cb53b9d122e312b95d45
AshimChoudhary <panghalashim7832@gmail.com>
2025-08-17 22:14:19 +0530

    Setting up of TMDB

--- server/routes/ShowRoute.js
+++ server/routes/ShowRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getNowPlayingMovies } from '../controllers/showController.js';
+import { addShow, getNowPlayingMovies } from '../controllers/showController.js';
 
 const ShowRouter = express.Router();
 
 ShowRouter.get('/now-playing', getNowPlayingMovies);
+ShowRouter.post('/add', addShow);
 
 export default ShowRouter;
