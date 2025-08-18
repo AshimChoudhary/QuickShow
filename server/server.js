111d0594b866f056d077edddaa68a2048d65cfa4
AshimChoudhary <panghalashim7832@gmail.com>
2025-08-18 17:27:40 +0530

    Now the Admin Check Phase

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkMiddleware } from '@clerk/express';
 import { serve } from 'inngest/express';
 import { inngest, functions } from './inngest/index.js';
 import ShowRouter from './routes/ShowRoute.js';
+import bookingRouter from './routes/bookingRoutes.js';
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(clerkMiddleware());
 app.get('/', (req, res) => res.send('Server is Live!'));
 app.use('/api/inngest', serve({ client: inngest, functions }));
 app.use('/api/show', ShowRouter);
+app.use('/api/booking', bookingRouter);
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
